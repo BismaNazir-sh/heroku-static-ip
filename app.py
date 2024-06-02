628d1fa876b0bb9d3a89adb27daa50d814eddc7b
BismaNazir-sh <156180983+BismaNazir-sh@users.noreply.github.com>
2024-06-02 15:17:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,23 +9,26 @@ try:
 except:
     pass
 
-MONGODB_URI = env.str('MONGODB_URI', default="mongodb+srv://Bisma:Bisma123@cluster0.r1tthak.mongodb.net/")
+try:
+    MONGODB_URI = env.str('MONGODB_URI', default="mongodb+srv://Bisma:Bisma123@cluster0.r1tthak.mongodb.net/")
 
-client = MongoClient(MONGODB_URI)
-db = client['sea-turtle']
+    client = MongoClient(MONGODB_URI)
+    db = client['sea-turtle']
 
-def get_data():
-    # Replace 'drugs' with the name of your collection
-    collection = db['drugs']
-    data = collection.find()
-    data_list = list(data)
-    for item in data_list:
-        item['_id'] = str(item['_id'])  # Convert ObjectId to string
-        print('item', item)
-    return
+    def get_data():
+        # Replace 'drugs' with the name of your collection
+        collection = db['drugs']
+        data = collection.find()
+        data_list = list(data)
+        for item in data_list:
+            item['_id'] = str(item['_id'])  # Convert ObjectId to string
+            print('item', item)
+        return
 
-if __name__ == '__main__':
-    get_data()
+    if __name__ == '__main__':
+        get_data()
+except:
+    pass
 
 
 import os
