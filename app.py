3ba678cae1e116449d5b4c0dfab2ecc6c538df19
BismaNazir-sh <156180983+BismaNazir-sh@users.noreply.github.com>
2024-06-03 17:09:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,17 @@ MONGODB_URI = env.str('MONGODB_URI', default="mongodb+srv://Bisma:Bisma123@clust
 #MONGODB_URI = "mongodb+srv://Bisma:Bisma123@cluster1.lham6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
 def get_data():
     try:
+        print("in try")
         # Connect to MongoDB
         client = pymongo.MongoClient(
             MONGODB_URI
         )
         db = client['sample_geospatial']
-
+        print('got db')
         # Replace 'drugs' with the name of your collection
         collection = db['shipwrecks']
         data = collection.find()
+        print("got collection")
         data_list = list(data)
         for item in data_list:
             item['_id'] = str(item['_id'])  # Convert ObjectId to string
